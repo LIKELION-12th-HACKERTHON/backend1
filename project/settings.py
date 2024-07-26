935047d1e5b4a7a1bb43278675587f2976aba177
soka03 <soka27@naver.com>
2024-07-27 01:42:45 +0900

    A

--- project/settings.py
+++ project/settings.py
@@ -77,7 +77,6 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:5173",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
     "https://ourvege.shop"]
 
 CORS_ALLOW_CREDENTIALS = True
